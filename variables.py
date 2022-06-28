c4c9345c53193005800a398ea2b998316154998f
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-28 10:17:49 +0100

    Typecasting

--- variables.py
+++ variables.py
@@ -55,3 +55,28 @@ print(type(True))
 ›<class 'int'>
 ›<class 'float'>
 ›<class 'bool'>
+
+#Typcasting
+a = int(1)        # a will be 1
+b = int(2.5)      # b will be 2
+c = int("3")      # c will be 3
+d = float(1)      # d will be 1.0
+e = float(2.5)    # e will be 2.5
+f = float("3")    # f will be 3.0
+g = float("4.23") # g will be 4.23
+h = str("80s")    # h will be '80s'
+i = str(22)       # i will be '22'
+j = str(3.01)     # j will be '3.01'
+
+print([a,b,c,d,e,f,g,h,i,j])
+
+#console
+›[1, 2, 3, 1.0, 2.5, 3.0, 4.23, '80s', '22', '3.01']
+
+#note
+c1 = int("3.4")   # c1 will be...  ›Traceback (most recent call last): ValueError: invalid literal for int() with base 10: '3.4'
+                                    >!Error: Unknown error
+#except this
+c1 = int(float("3.4")) 
+#console
+>3
