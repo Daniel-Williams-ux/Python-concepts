9a7b83998f5f4a1fda989c7004af1243d7324f00
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-28 08:26:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -57,3 +57,14 @@ print("{} - {} = {}".format(num1, num2, difference))
 print("{} * {} = {}".format(num1, num2, product))
 print("{} / {} = {}".format(num1, num2, quotient))
 print("{} % {} = {}".format(num1, num2, remainder))
+
+
+# Problem: Receive miles and convert to kiometers
+# Kilometers = miles * 1.60934
+# Enter miles 5
+# 5 miles equas 8.04 kilometers
+
+miles = input('Enter miles ')
+miles = int(miles)
+kilometers = miles * 1.60934
+print("{} miles equals {} kilometers".format(miles, kilometers)) 
