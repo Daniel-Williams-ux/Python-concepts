cec514012b6445882eb375dcbe4c8fb5c4ac654f
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-28 10:07:45 +0100

    Update variables.py

--- variables.py
+++ variables.py
@@ -31,3 +31,27 @@ print(name + '  is doing well in geography.')
 a="it's"
 #or
 b='it\'s'
+
+
+#note here no 2.56 is not a string
+failed_subjects=2.56
+name='John'
+print('Dear Mrs Badger')
+print('Your son ' + name + ' is failing ' + str(failed_subjects) + ' subjects.')
+print(name + '  will need to redo ' + str(failed_subjects) + '  courses.')
+name="Eric"
+print(name + '  is doing well in geography.')
+print(type('hello'))
+print(type(1))
+print(type(1.64))
+print(type(True))
+
+#console
+›Dear Mrs Badger
+›Your son John is failing 2.56 subjects.
+›John will need to redo 2.56 courses.
+›Eric is doing well in geography.
+›<class 'str'>
+›<class 'int'>
+›<class 'float'>
+›<class 'bool'>
