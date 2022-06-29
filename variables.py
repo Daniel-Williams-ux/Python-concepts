6991928dc9abfcfe417ed4c3221c0275507f523b
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-29 15:20:23 +0100

    Update variables.py

--- variables.py
+++ variables.py
@@ -90,3 +90,23 @@ item_name = 'widget'
 price = 23.5
 inventory = 100
 print(item_name, price, inventory)
+
+
+#Arithmetic Operation
+a=10
+b=3
+print('Addition : ', a + b)
+print('Subtraction : ', a - b)
+print('Multiplication : ', a * b)
+print('Division (float) : ', a / b)
+print('Division (floor) : ', a // b)
+print('Modulus : ', a % b)
+print('Exponent : ', a ** b)
+#console
+›Addition : 13
+›Subtraction : 7
+›Multiplication : 30
+›Division (float) : 3.3333333333333335
+›Division (floor) : 3
+›Modulus : 1
+›Exponent
