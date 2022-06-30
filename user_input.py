f7e862e8432c7a1f3b663be052b1f75360b5efdd
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-01 00:14:32 +0100

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,28 @@
+name= input('What is your name?: ')
+print(name)
+#Console
+Daniel
+
+name = input("What is your name ")
+print("Hello " + name + "!")
+#console
+Hello Daniel!
+
+#Simple Calculator
+num1=input('Enter a digit: ')
+num2=input('Enter a second number:')
+answer=float(num1)+float(num2) #typecasting => converting string to integer
+print(answer)
+#console 
+2+2 = 4.0
+
+#or
+num1=input('Enter a digit: ')
+num1 = int(num1) #typecasting => converting string to integer
+num2=input('Enter a second number:')
+num2 = int(num2) #typecasting
+answer=(num1)+(num2)
+print(answer)
+#console 
+2+2 = 4
+
