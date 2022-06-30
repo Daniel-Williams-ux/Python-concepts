1bdc126b2980b95a193ef6faca9c21cea6bbcdc5
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-30 23:33:50 +0100

    Update strings.py

--- strings.py
+++ strings.py
@@ -71,3 +71,13 @@ print(msg[:7])
 >lcome to Python 101: Strings
 ›lcome
 ›welcome
+          
+          
+          
+msg='welcome to Python 101: Strings'
+msg1=msg[18]+' '+msg[:8]+msg[25:29]+msg[7:11]+msg[13]+msg[12]+msg[2]+msg[1]+msg[-5]  
+print(msg1.title())
+print(msg1[::-1].title()) #reverse the string
+#CONSOLE
+›1 Welcome Ring To Tyler
+›Relyt Ot Gnir Emoclew 1
