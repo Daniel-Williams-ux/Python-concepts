aea90d133d768cef27af58d83b6cf836be3974bb
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-22 09:20:38 +0100

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -77,3 +77,20 @@ else:
     print("30 divided by",num, "is: ", 30/num)
 finally:
     print("**Thank you for playing!**")
+
+#more
+try:
+    num=int(input('Enter a number between 1 and 30: '))
+    num1 = 30/num
+    if num > 30:
+        raise ValueError(num)
+except ZeroDivisionError as err:
+    print(err, "You can't divide by Zero!!!")
+except ValueError as err:
+    print(err,num, "Bad Value not between 1 and 30!")
+except:
+    print("Invalid Input!")
+else:
+    print("30 divided by",num, "is: ", 30/num)
+finally:
+    print("**Thank you for playing!**")
