90f2dac3f4bc070c710a09346b2e74c40715ce86
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-25 16:52:46 +0100

    Update classes_and_objects.py

--- classes_and_objects.py
+++ classes_and_objects.py
@@ -19,3 +19,27 @@ print(film_1.title, film_1.imdb_score)
 
 #What is __init__?
 #The instance method called when a new object is created
+
+
+class Movie:
+    def __init__(self,title,year,imdb_score,have_seen):
+        self.title = title
+        self.year = year
+        self.imdb_score = imdb_score
+        self.have_seen = have_seen
+    
+    def nice_print(self):
+        print("Title: ", self.title)
+        print("Year of production: ", self.year)
+        print("IMDB Score: ", self.imdb_score)
+        print("I have seen it: ", self.have_seen)
+        
+film_1 = Movie("Life of Brian",1979,8.1,True)
+film_2 = Movie("The Holy Grail",1975,8.2,True)
+
+film_2.nice_print()
+#CONSOLE
+#›Title: The Holy Grail
+#›Year of production: 1975
+#›IMDB Score: 8.2
+#›I have seen it: True
