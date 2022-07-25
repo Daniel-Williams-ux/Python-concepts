d291bc0515c7af65c53c7f71ec4249d0523375bf
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-25 16:56:36 +0100

    Update classes_and_objects.py

--- classes_and_objects.py
+++ classes_and_objects.py
@@ -45,3 +45,30 @@ Movie.nice_print(film_2)
 #›Year of production: 1975
 #›IMDB Score: 8.2
 #›I have seen it: True
+
+
+class Movie:
+    def __init__(self,title,year,imdb_score,have_seen):
+        self.title = title
+        self.year = year
+        self.imdb_score = imdb_score
+        self.have_seen = have_seen
+    
+    def nice_print(self):
+        print("Title: ", self.title)
+        print("Year of production: ", self.year)
+        print("IMDB Score: ", self.imdb_score)
+        print("I have seen it: ", self.have_seen)
+        
+film_1 = Movie("Life of Brian",1979,8.1,True)
+film_2 = Movie("The Holy Grail",1975,8.2,True)
+
+films = [film_1,film_2]
+print(films[1].title,films[0].title)
+films[0].nice_print()
+#CONSOLE
+#›Title: The Holy Grail
+#›Year of production: 1975
+#›IMDB Score: 8.2
+#›I have seen it: True
+
