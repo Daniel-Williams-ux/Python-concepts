2c09e658ced9220e0d655c223043fb871ff4ac9b
Daniel Williams <williamsoligodaniel@gmail.com>
2022-08-07 08:01:09 +0100

    codecademy

--- Changing _Numbers.py
+++ Changing _Numbers.py
@@ -26,4 +26,26 @@ print(number_of_coffees)
 #We create two variables and assign numeric values to them. Then we perform a calculation on them. 
 #This doesn’t update the variables! When we update the coffee_price variable and perform the calculations again, they use the updated values for the variable!
 
+"""Instructions
+1.
+You’ve decided to get into quilting! To calculate the number of squares you’ll need for your first quilt let’s create two variables: 
+quilt_width and quilt_length. Let’s make this first quilt 8 squares wide and 12 squares long.
+
+Checkpoint 2 Passed
+2.
+Print out the number of squares you’ll need to create the quilt!
+
+Checkpoint 3 Passed
+3.
+It turns out that quilt required a little more material than you have on hand! Let’s only make the quilt 8 squares long. How many squares 
+will you need for this quilt instead?"""
+
+quilt_width = 8
+quilt_length = 12
+
+print(quilt_width * quilt_length)
+
+quilt_length = 8
+print(quilt_width * quilt_length)
+
 #source: codecademy
